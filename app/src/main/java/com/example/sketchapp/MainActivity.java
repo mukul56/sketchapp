ff4e1b2b8cb214c03ff457ccdb3a6ca5e5d45e52
mukul56 <amukul56@gmail.com>
2020-09-24 00:45:22 +0530

    Undo, Redo, Erase, Color change, Size-change, Authentication implemented

---
+++ app/src/main/java/com/example/sketchapp/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.sketchapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Stack;
+
+public class MainActivity extends AppCompatActivity {
+    MyCanvas myCanvas;
+    Button clear,undo,red,sizeM,sizeL,redo,green,save,eraser,pen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myCanvas = (MyCanvas) findViewById(R.id.canvasSing);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        myCanvas.initialise(displayMetrics);
+
+        clear = (Button) findViewById(R.id.clear);
+        undo = (Button) findViewById(R.id.undo);
+        red = (Button) findViewById(R.id.red);
+        sizeM = (Button) findViewById(R.id.sizeM);
+        sizeL = (Button) findViewById(R.id.sizeL);
+        save = (Button) findViewById(R.id.save);
+        green = (Button) findViewById(R.id.green);
+        redo = (Button) findViewById(R.id.redo);
+        pen = (Button) findViewById(R.id.pen);
+        eraser = (Button) findViewById(R.id.erase);
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas.clear();
+            }
+        });
+
+        undo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas.undo();
+            }
+        });
+
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas.setColor(Color.RED);
+            }
+        });
+
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas.setColor(Color.GREEN);
+            }
+        });
+
+        sizeM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas.setStrokeWidth(8);
+            }
+        });
+        sizeL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas.setStrokeWidth(12);
+            }
+        });
+
+        redo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas.redo();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        eraser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas.setErase(true);
+            }
+        });
+
+        pen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas.setErase(false);
+                myCanvas.setColor(Color.BLACK);
+            }
+        });
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/sketchapp/authentication/Authen.java
@@ -0,0 +1,40 @@
+package com.example.sketchapp.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.sketchapp.R;
+
+public class Authen extends AppCompatActivity {
+    private EditText editTextMobile;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authen);
+        editTextMobile = findViewById(R.id.editTextMobile);
+
+        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mobile = editTextMobile.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    editTextMobile.setError("Enter a valid mobile");
+                    editTextMobile.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(Authen.this, Verify.class);
+                intent.putExtra("mobile", mobile);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
